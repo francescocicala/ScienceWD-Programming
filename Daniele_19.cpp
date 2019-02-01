84f3d9aafccbfd23731735b7f78ec7bbdbb8c8cd
Ismaele <35141082+mechanapoleon@users.noreply.github.com>
2019-02-01 17:23:39 +0100

    Update Daniele_19.cpp

--- Daniele_19.cpp
+++ Daniele_19.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	int   counter = {1};
-	int   sign    = {1};
-	float total   = {1};
+    int   counter = {1};
+    int   sign    = {1};
+    float total   = {1};
 	
-	for ( counter = 1 ; counter <= 1000 ; counter = counter + 1 )
-	{
-            sign = - sign ;
-	    total = total  + sign *  1.0 / ( counter * 2.0 + 1.0) ;
+    for ( counter = 1 ; counter <= 1000 ; counter = counter + 1 )
+    {
+       sign = - sign ;
+       total = total  + sign *  1.0 / ( counter * 2.0 + 1.0) ;
     }
     
     cout << "pi=" << 4 * total  << endl;
